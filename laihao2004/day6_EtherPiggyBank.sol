2ad3696f4dc84f7af8c586112d76ea447bc26a85
laihao2004 <laih9776@gmail.com>
2026-03-07 23:21:16 +0800

    提交人laihao2004 挑战尝试闯关到Day6 (#249)
    * Create day1_ClickCounter.sol
    
    * Create day2_SaveMyName.sol
    
    * Create day3_PollStation.sol
    
    * Create day4_AuctionHouse.sol
    
    * Create day5_AdminOnly.sol
    
    * Create day6_EtherPiggyBank.sol
    
    * Create day7_SimpleIOUApp

---
+++ laihao2004/day6_EtherPiggyBank.sol
@@ -0,0 +1,56 @@
+//SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8.0;
+
+contract EtherPiggyBank{
+
+    address public bankManager;
+    address[] members;
+    mapping(address => bool) public registeredMembers;
+    mapping(address => uint256) balance;
+
+    constructor(){
+        bankManager = msg.sender;
+        members.push(msg.sender);
+        registeredMembers[msg.sender] = true;
+    }
+
+    modifier onlyBankManager(){
+        require(msg.sender == bankManager, "Only bank manager can perform this action");
+        _;
+    }
+
+    modifier onlyRegisteredMember() {
+        require(registeredMembers[msg.sender], "Member not registered");
+        _;
+    }
+  
+    function addMembers(address _member)public onlyBankManager{
+        require(_member != address(0), "Invalid address");
+        require(_member != msg.sender, "Bank Manager is already a member");
+        require(!registeredMembers[_member], "Member already registered");
+        registeredMembers[_member] = true;
+        members.push(_member);
+    }
+
+    function getMembers() public view returns(address[] memory){
+        return members;
+    }
+    function depositAmountEther() public payable onlyRegisteredMember{  
+        require(msg.value > 0, "Invalid amount");
+        balance[msg.sender] = balance[msg.sender]+msg.value;
+   
+    }
+    
+    function withdrawAmount(uint256 _amount) public onlyRegisteredMember{
+        require(_amount > 0, "Invalid amount");
+        require(balance[msg.sender] >= _amount, "Insufficient balance");
+        balance[msg.sender] = balance[msg.sender]-_amount;
+   
+    }
+
+    function getBalance(address _member) public view returns (uint256){
+        require(_member != address(0), "Invalid address");
+        return balance[_member];
+    } 
+}
